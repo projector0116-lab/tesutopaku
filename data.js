b029c0614198fd43c695d0402494559b629e19d8
artisan <projector.0116@gmail.com>
2026-06-18 15:19:57 +0900

    Delete data.js

--- data.js
+++
@@ -1,6 +0,0 @@
-const testData = [
-  { "id": 1, "title": "社会：時差の基本（1〜5問）", "status": "todo" },
-  { "id": 2, "title": "社会：時差の計算（6〜10問）", "status": "todo" },
-  { "id": 3, "title": "社会：時差の応用（11〜15問）", "status": "todo" },
-  { "id": 4, "title": "過去問：世界と日本", "status": "done" }
-];
